01fbf703a2b1b47f6775efac72934b3a09bd7022
JarsBr <josejarsbr@gmail.com>
2023-04-05 09:19:46 -0300

    05/04 09:19

---
+++ SmartLibrary/src/Teste/idex.java
@@ -0,0 +1,43 @@
+package Teste;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class idex {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					idex window = new idex();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public idex() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
