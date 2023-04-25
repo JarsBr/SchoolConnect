39d202a187f7aac7b9c96bea1db14c2026a2fc7a
Ítalo Gonçalves <italogmfaria@gmail.com>
2023-04-25 10:37:20 -0300

    1.1
    arroba

--- Projeto/src/br/com/schoolconnect/projeto/controller/ControllerInterfaceAluno.java
+++ Projeto/src/br/com/schoolconnect/projeto/controller/ControllerInterfaceAluno.java
@@ -3,34 +3,20 @@ package br.com.schoolconnect.projeto.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 public class ControllerInterfaceAluno {
 
-	@FXML
-	private ImageView config;
+    @FXML
+    private Button iniciar;
 
-	@FXML
-	private Button create;
+    @FXML
+    private Pane panel;
 
-	@FXML
-	private Pane panel;
+    @FXML
+    void iniciar(ActionEvent event) {
 
-	@FXML
-	void iniciar(ActionEvent event) {
-
-	}
-
-	@FXML
-	void nununud0s(MouseEvent event) {
-		System.out.println("Teste Tela Aluno");
-	}
-
-	@FXML
-	void panel(MouseEvent event) {
-
-	}
+    }
 
 }
+
